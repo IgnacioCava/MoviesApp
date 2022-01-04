16f111ddbd10d885f103a61bc22e8510b9f38875
IgnacioCava <cavaignacio11@gmail.com>
2022-01-04 03:08:59 -0300

    Styled movie cards a bit more. Will add detailed information to them later

--- src/Components/Movie.jsx
+++ src/Components/Movie.jsx
@@ -1,31 +1,107 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
+import { getDetails } from '../Actions'
 
-export default function Movie({title,year, poster}){
+export default function Movie({title,year,poster,id}){
+
+    const dispatch=useDispatch()
+
+    
+    
+
+
+    // const Detail = useSelector(state=>state.movieDetail)
+
+    // // var Detail = {
+    // //     runtime : json.Runtime,
+    // //     genero: json.Genre,
+    // //     plot: json.Plot,
+    // //     actores: json.Actors,
+    // //     rating: {
+    // //         imbd: json.Ratings[0].Value,
+    // //         RT: json.Ratings[1].Value,
+    // //         Metacritic: json.Ratings[2].Value
+    // //     }
+
+    // // }
+
+    // console.log(Detail)
 
     return(
         <MovieDisplay>
-            <p style={{margin:'auto'}}>{title}</p>
             
-            {/*year*/}
-            
-            
-            <img style={{height:'available', width: '100%'}} src={poster}/>
-            
+            <SmallDetail>
+                {title}
+                <p>Criptopingolarororororrorrororoororororoor</p>
+            </SmallDetail>
+
+            <img style={{height:'100%', width: '100%'}} src={poster}/>
+            <Title>{title}</Title>
+
         </MovieDisplay>
     )
 }
 
+const SmallDetail = styled.div`
+    position:absolute; 
+    word-break:break-word;
+    top:0;
+    height:100%;
+    width:100%;
+    padding: 5px;
+    box-sizing: border-box;
+    backdrop-filter: blur(1px) brightness(60%);
+    text-overflow: clip;
+    background:linear-gradient(rgb(1,1,1,.7) 20%, transparent);
+`
+
+const Title = styled.h2`
+    position: absolute;
+    padding:5px;
+    padding-top: 10px;
+    box-sizing: border-box;
+    width: 100%;
+    height: fit-content;
+    min-height: 15%;
+    background:linear-gradient(transparent, black 20%);
+    font-size: .875rem;
+    font-weight: 500;
+    color:white;
+    margin:0;
+    content: 'xd';
+`
+
 const MovieDisplay = styled.div`
-display:flex;
-flex-direction: column-reverse;
-width: 15%;
-margin:10px;
-justify-content: flex-end;
-min-width: 150px;
-border:3px solid black;
-border-radius: 15px;
-overflow: hidden;
+    position: relative;
+    display:flex;
+    align-items: center;
+    justify-content: flex-end;
+    flex-direction: column;
+    width: 16%;
+    margin:10px;
+    border-radius: 15px;
+    min-width: 150px;
+    overflow: hidden;
+    transition:.4s;
+    
+    > ${SmallDetail}{
+            transition:.3s;
+            visibility: hidden;
+            opacity:0;
+            color: white
+        }
+    &:hover{
+        
+        transform: scale(105%);
+        > ${Title}{
+            filter: blur(4px);
+            transition:.4s;
+        }
+        > ${SmallDetail}{
+            visibility: visible;
+            opacity:1;
+        }
+    }
 `
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import Movie from './Movie'
 import styled from 'styled-components'
 
