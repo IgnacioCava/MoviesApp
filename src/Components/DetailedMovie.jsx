b792a52abc392940e5019f0fa5266f1c19eb2fe3
IgnacioCava <cavaignacio11@gmail.com>
2022-02-23 23:51:48 -0300

    x

--- src/Components/DetailedMovie.jsx
+++ src/Components/DetailedMovie.jsx
@@ -25,17 +25,28 @@ export default function DetailedMovie(){
         return (
             <DetailWrapper>
                 
+                <div style={{zIndex:0, position:'absolute', top:0, height:'100%', width:'100%', background:'#0707318b'}}></div>
                 <Backdrop src={`https://image.tmdb.org/t/p/original/${thisDetail.backdrop_path}`} alt='backdrop'/>
+                
                 <Detail>
-                    <p>{thisDetail.original_title}</p>
-                    <p>{thisDetail.tagline}</p>
-                    <p>{thisDetail.overview}</p>
-                    {thisDetail.genres.map(e=>{
-                                    return <p key={id+e.name}>{e.name}</p>
-                                })}
-                    <p>IMDb: {thisDetail.vote_average}/10</p>
-                    <a href={thisDetail.homepage}><p>{thisDetail.homepage}</p></a>
-                    <Link to='/'>Back</Link>
+                    <img src={'https://image.tmdb.org/t/p/w300/'+thisDetail.poster_path} alt='poster'/>
+                    
+                    <DetailText>
+                        <Title>{thisDetail.original_title}</Title>
+                        <p style={{margin:0}}>{thisDetail.tagline}</p>
+                        <p>IMDb: {thisDetail.vote_average}/10 | {thisDetail.runtime} min {thisDetail.release_date.split('-')[0]}</p>
+                        <Overview>{thisDetail.overview}</Overview>
+                        {thisDetail.genres.map(e=>{
+                                        return <Genre key={id+e.name}>{e.name}</Genre>
+                                    })}
+                        
+                        {thisDetail.production_companies.map(e=>{
+                                        if(e.logo_path) return <img style={{width:'20%'}} src={'https://image.tmdb.org/t/p/original/'+e.logo_path} alt='logo' key={e.name}/>
+                                        else return <span>{e.name}</span>
+                                    })}
+                        <a href={thisDetail.homepage}><p>{thisDetail.homepage}</p></a>
+                        <Link to='/'>Back</Link>
+                    </DetailText>
                 </Detail>
             </DetailWrapper>
         )
@@ -45,13 +56,41 @@ export default function DetailedMovie(){
     
 }
 
-const Detail = styled.div`
+const Overview = styled.p`
+color: #8da0bc;
+`
+
+const Genre = styled.span`
+    background-color: #1c2e57;
+    border-radius:5px;
+    margin-right:5px;
+    padding: 0 5px 0 5px;
+    width: fit-content;
+    line-height: 1.5rem;
+    color: #89abb6;
+`
+
+const Title = styled.p`
+    color: #c6c6d3;
+    font-size:xx-large;
+    font-weight: bolder;
+    margin-bottom:0px;
+    margin-top:0px;
+`//
+
+const DetailText = styled.div`
     margin-left:15px;
     display: flex;
     flex-direction: column;
-    background-color:rgba(1,1,1,.8);
-    max-width: 30%;
     text-align:left;
+`
+
+const Detail = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    text-align:left;
+    z-index: 2;
 `
 
 const DetailWrapper = styled.div`
@@ -59,14 +98,15 @@ const DetailWrapper = styled.div`
     display: flex;
     height: 100%;
     width: 100%;
+    justify-content: center;
 `
 
 const Backdrop = styled.img`
-position:absolute;
-left:0;
-top:0;
-width: 100%;
-height: 100vh;
-object-fit: cover;
-z-index: -1;
+    position:absolute;
+    width: 100%;
+    height: 100%;
+    top:0;
+    object-fit: cover;
+    filter: brightness(90%);
+    z-index: -1;
 `
